3be91d590ccb331f2ee6f52eea27cbf2cfba9133
Kasir <kasir.barati@gmail.com>
2022-06-26 11:24:00 +0430

    add: conditional and loop sample

---
+++ first-nirvana/conditional-statements.py
@@ -0,0 +1,14 @@
+if True:
+    print("if")
+
+if False:
+    print("false")
+elif True:
+    print("if...elif")
+
+if False:
+    print("false")
+elif False:
+    print("true")
+else:
+    print("if..elif...else")
---
+++ first-nirvana/loop.py
@@ -0,0 +1,19 @@
+names = ['Kasir','Mohammad Jawad']
+
+for name in names:
+    print(name)
+
+start = 100
+end = 200
+step = 30
+for index in range(start, end, step):
+    print(index)
+
+while start <= end:
+    print(step)
+    start = start + step
+
+"""
+    Wrong usage: 
+    - start += step
+"""
