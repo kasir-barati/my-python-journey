2fa5c921eada9ff10d27dc23492093107df2803d
Kasir <kasir.barati@gmail.com>
2022-06-29 20:28:55 +0430

    update: http POST method

--- forth_mandala/http_requests.py
+++ forth_mandala/http_requests.py
@@ -2,6 +2,7 @@ import simplejson as json
 import requests
 from io import BytesIO
 from PIL import Image
+from .types.create_user import CreateUser
 
 
 def fetchJsonData(url: str) -> None:
@@ -47,5 +48,27 @@ def download_picture(url: str) -> None:
         print("Cannot save file")
 
 
+def create_user(url: str, data: CreateUser) -> None:
+    """
+    We do not need to specify the data type as it is detected by requests
+    and the proper http content-type header would be settled.
+    """
+    response = requests.post(url, data)
+    
+    """
+    The response.content is 
+      b'{"name":"Kasir","job":"Fullstack developer in Germany","id":"175","createdAt":"2022-06-29T15:53:28.988Z"}'
+    as you know we can deserialize it with json.loads
+    """
+    print(json.loads(response.content))
+
 fetchJsonData("https://reqres.in/api/users")
 download_picture("https://mcdn.wallpapersafari.com/medium/79/6/BcX5K7.jpg")
+create_user(
+    "https://reqres.in/api/users", 
+    {
+        "name": "Kasir",
+        "job": "Fullstack developer in Germany"
+    }
+)
+
