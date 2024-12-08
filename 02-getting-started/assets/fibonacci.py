65b17052b804d2660af9176e92b2a440bd134314
Kasir Barati <kasir.barati@gmail.com>
2024-12-08 16:25:24 +0100

    chore: cleanup

--- 02-getting-started/assets/fibonacci.py
+++ 02-getting-started/assets/fibonacci.py
@@ -15,7 +15,7 @@ def fib2(number):
     result = []
     f0, f1 = 0, 1
     while f0 < number:
-        result.append(f0)    # see below
+        result.append(f0)
         f0, f1 = f1, f0+f1
     return result
 
