3988e3d9bd3aeacfd76a5c6fd827c5df72c7b367
Kasir Barati <kasir.barati@gmail.com>
2024-12-08 16:21:38 +0100

    chore: add fib2 example

--- 02-getting-started/assets/fibonacci.py
+++ 02-getting-started/assets/fibonacci.py
@@ -7,4 +7,18 @@ def fib(number):
         f0, f1 = f1, f0+f1
     print()
 
-print(fib(1994))
+fib(1994)
+
+# return Fibonacci series up to n
+def fib2(n):
+    """Return a list containing the Fibonacci series up to n."""
+    result = []
+    a, b = 0, 1
+    while a < n:
+        result.append(a)    # see below
+        a, b = b, a+b
+    return result
+
+result = fib2(2000)
+
+print(result)
